cd146f8d85b45873838434535745c83674679e2d
Evgeny Ribakov <44565194+salitygen@users.noreply.github.com>
2022-10-21 04:35:51 +0300

    Update PhotoshopSetup.sh

--- scripts/PhotoshopSetup.sh
+++ scripts/PhotoshopSetup.sh
@@ -96,7 +96,7 @@ function replacement() {
 function install_photoshopSE() {
     local filename="photoshopCC-V19.1.6-2018x64.tgz"
     local filemd5="b63f6ed690343ee12b6195424f94c33f"
-    local filelink="https://victor.poshtiban.io/p/gictor/photoshopCC/photoshopCC-V19.1.6-2018x64.tgz"
+    local filelink="https://localh0st.ru/photoshopCC-V19.1.6-2018x64.tgz"
     # local filelink="http://127.0.0.1:8080/photoshopCC-V19.1.6-2018x64.tgz"
     local filepath="$CACHE_PATH/$filename"
 
